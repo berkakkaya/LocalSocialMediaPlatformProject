1fb9efc16480ba8645525f26dece1bf4e8f7ef04
Berk Akkaya <berkakkaya000@gmail.com>
2022-12-18 22:22:30 +0300

    add: variables to NormalGonderi

--- src/gonderi/NormalGonderi.java
+++ src/gonderi/NormalGonderi.java
@@ -5,4 +5,8 @@ import kullanici.Kullanici;
 import java.util.ArrayList;
 
 public class NormalGonderi extends Gonderi {
+    private final ArrayList<Integer> begenenler;
+    private final String icerik;
+    private int yenidenPaylasmaSayisi;
+
 }
