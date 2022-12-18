c956b921830632757f172cd539f3e38a48b6a0e4
Berk Akkaya <berkakkaya000@gmail.com>
2022-12-18 22:22:55 +0300

    add: constructor for NormalGonderi

--- src/gonderi/NormalGonderi.java
+++ src/gonderi/NormalGonderi.java
@@ -9,4 +9,11 @@ public class NormalGonderi extends Gonderi {
     private final String icerik;
     private int yenidenPaylasmaSayisi;
 
+    public NormalGonderi(Kullanici kullanici, String icerik) {
+        super(kullanici);
+        this.icerik = icerik;
+        this.begenenler = new ArrayList<>();
+        this.yenidenPaylasmaSayisi = 0;
+    }
+
 }
