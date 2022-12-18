85b9d0866f3c58f965f4555f11d25618f5e56640
Berk Akkaya <berkakkaya000@gmail.com>
2022-12-18 19:48:33 +0300

    Add variables for Veritabani class

--- src/veritabani/Veritabani.java
+++ src/veritabani/Veritabani.java
@@ -1,4 +1,11 @@
 package veritabani;
 
+import gonderi.Gonderi;
+import kullanici.Kullanici;
+
+import java.util.ArrayList;
+
 public class Veritabani {
+    private static final ArrayList<Gonderi> gonderiler = new ArrayList<>();
+    private static final ArrayList<Kullanici> kullanicilar = new ArrayList<>();
 }
