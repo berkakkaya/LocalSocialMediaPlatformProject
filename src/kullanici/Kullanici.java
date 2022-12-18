3bdf8cd2ff91b3fa07e9a0227ecc91fc023f2d34
İrem Akkın <iremakkin1453@gmail.com>
2022-12-18 14:01:37 +0300

    "Kullanici constructor yapısını tamamla"
    Kullanici constructor yapısını tamamladım.

--- src/kullanici/Kullanici.java
+++ src/kullanici/Kullanici.java
@@ -1,11 +1,23 @@
 package kullanici;
 
+import hatalar.SifreEslesmiyorException;
+
 public class Kullanici{
     private final int kullaniciNumarasi;
     private String adSoyad;
     private String ePosta;
     private final String kullaniciAdi;
     private String sifre;
+
+    public Kullanici(String adSoyad, String ePosta, String kullaniciAdi, String sifre, String sifreYeniden ){
+        this.adSoyad = adSoyad;
+        this.ePosta = ePosta;
+        this.kullaniciAdi = kullaniciAdi;
+        this.sifre = sifre;
+        if(sifreYeniden != sifre){
+           throw new SifreEslesmiyorException("Yazdığınız şifre, ilk yazdığınız şifre ile uyuşmuyor.");
+        }
+    }
 
     public String getAdSoyad() {
         return adSoyad;
